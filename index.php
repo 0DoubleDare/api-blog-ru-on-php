3585c39bbf13bd6221fb0e2ca25ea0c4cd134361
0DoubleDare <lidernikita905@gmail.com>
2026-03-31 09:19:27 +0400

    Update folders & code

--- index.php
+++ index.php
@@ -6,14 +6,13 @@ require 'models/model.php';
 
 $method = $_SERVER['REQUEST_METHOD'];
 $params = explode('/', $_GET['q']);
+$type = $params[0];
+if (isset($params[1])) {
+    $id = $params[1];
+}
 
 switch($method) {
     case 'GET':
-//        $params = explode('/', $_GET['q']);
-        $type = $params[0];
-        if (isset($params[1])) {
-            $id = $params[1];
-        }
         if ($type === 'posts') {
             if (isset($id)) {
                 GetPost(connectDatabase(), $id);
@@ -23,21 +22,21 @@ switch($method) {
         }
         break;
     case 'POST':
-//        $params = explode('/', $_GET['q']);
 //        echo json_encode($_POST);
 //        print_r($params);
 
         AddPost(connectDatabase(), $_POST);
         break;
     case 'PATCH':
-//        $params = explode('/', $_GET['q']);
-        $id = $params[1];
-        $data = file_get_contents("php://input");
-        $data = json_decode($data, true);
-        UpdatePost(connectDatabase(), $id, $data);
+        if ($type === 'posts') {
+            $data = file_get_contents("php://input");
+            $data = json_decode($data, true);
+            UpdatePost(connectDatabase(), $id, $data);
+        }
         break;
     case 'DELETE':
-        $id = $params[1];
-        DeletePost(connectDatabase(), $id);
+        if ($type === 'posts') {
+            DeletePost(connectDatabase(), $id);
+        }
         break;
 }
